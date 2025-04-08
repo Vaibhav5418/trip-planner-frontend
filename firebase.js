e8b1b617bbe1a7720ce22e826d0fa34ff90aaade
Vaibhav5418 <sonivaibhav037@gmail.com>
2025-04-09 00:49:03 +0530

    Initial frontend commit

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// frontend/firebase.js
+const firebaseConfig = {
+    apiKey: "AIzaSyAavcxc2GL5QqsUoKQNU47XFEsXNXhvcSI",
+    authDomain: "trip-planner-ai-a96eb.firebaseapp.com",
+    projectId: "trip-planner-ai-a96eb",
+    storageBucket: "trip-planner-ai-a96eb.appspot.com",
+    messagingSenderId: "615971832074",
+    appId: "1:615971832074:web:9f4b5558352131cb47baf3"
+  };
+  
+  // Initialize Firebase (compat)
+  firebase.initializeApp(firebaseConfig);
+  
